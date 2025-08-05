0002e55634a1a4a3a7b1c5ef4e2db9ad088976d3
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-08-05 12:40:55 +0200

    add: footer

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { View } from 'react-native';
+import LittleLemonFooter from './components/LittleLemonFooter';
 import LittleLemonHeader from './components/LittleLemonHeader';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#495E57',
-      }}>
-      <LittleLemonHeader />
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-between",
+          backgroundColor: '#495E57'
+        }}>
+        <LittleLemonHeader />
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
