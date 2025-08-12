9de89c40afabed0626e5701e1bfdc052b71e2125
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-08-12 11:54:24 +0200

    add: section list and remove flat list

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,27 +1,47 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
 const menuItemsToDisplay = [
-    { name: 'Hummus', price: '$5.00', id: '1A' },
-    { name: 'Moutabal', price: '$5.00', id: '2B' },
-    { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$3.00', id: '11K' },
-    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-    { name: 'Baklava', price: '$3.00', id: '18S' },
-    { name: 'Tartufo', price: '$3.00', id: '19T' },
-    { name: 'Tiramisu', price: '$5.00', id: '20U' },
-    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+    {
+        title: 'Appetizers',
+        data: [
+            { name: 'Hummus', price: '$5.00' },
+            { name: 'Moutabal', price: '$5.00' },
+            { name: 'Falafel', price: '$7.50' },
+            { name: 'Marinated Olives', price: '$5.00' },
+            { name: 'Kofta', price: '$5.00' },
+            { name: 'Eggplant Salad', price: '$8.50' },
+        ],
+    },
+    {
+        title: 'Main Dishes',
+        data: [
+            { name: 'Lentil Burger', price: '$10.00' },
+            { name: 'Smoked Salmon', price: '$14.00' },
+            { name: 'Kofta Burger', price: '$11.00' },
+            { name: 'Turkish Kebab', price: '$15.50' },
+        ],
+    },
+    {
+        title: 'Sides',
+        data: [
+            { name: 'Fries', price: '$3.00', id: '11K' },
+            { name: 'Buttered Rice', price: '$3.00' },
+            { name: 'Bread Sticks', price: '$3.00' },
+            { name: 'Pita Pocket', price: '$3.00' },
+            { name: 'Lentil Soup', price: '$3.75' },
+            { name: 'Greek Salad', price: '$6.00' },
+            { name: 'Rice Pilaf', price: '$4.00' },
+        ],
+    },
+    {
+        title: 'Desserts',
+        data: [
+            { name: 'Baklava', price: '$3.00' },
+            { name: 'Tartufo', price: '$3.00' },
+            { name: 'Tiramisu', price: '$5.00' },
+            { name: 'Panna Cotta', price: '$5.00' },
+        ],
+    },
 ];
 
 const Item = ({ name, price }) => {
@@ -31,17 +51,27 @@ const Item = ({ name, price }) => {
             <Text style={menuStyles.text}>{price}</Text>
         </View>
     )
-}
+};
+
+const Header = () => <Text style={menuStyles.title}>Menu list</Text>
 
 export default function MenuItems() {
     const renderItem = ({ item }) => <Item name={item.name} price={item.price} />
 
+    const renderSectionHeader = ({ section: { title } }) => (
+        <View>
+            <Text style={menuStyles.sectionTitle}>{title}</Text>
+        </View>
+    )
+
     return (
         <View style={menuStyles.container}>
-            <FlatList
-                data={menuItemsToDisplay}
+            <SectionList
+                sections={menuItemsToDisplay}
                 renderItem={renderItem}
-                keyExtractor={renderItem => renderItem.id}
+                keyExtractor={(item, index) => item + index}
+                ListHeaderComponent={Header}
+                renderSectionHeader={renderSectionHeader}
             />
         </View>
     )
@@ -61,5 +91,18 @@ const menuStyles = StyleSheet.create({
     text: {
         color: "#F4CE14",
         fontSize: 20
+    },
+    title: {
+        color: "#F4CE14",
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold",
+        textDecorationLine: "underline",
+        paddingBottom: 20
+    },
+    sectionTitle: {
+        backgroundColor: "#F4CE14",
+        textAlign: "center",
+        fontSize: 25
     }
 });
